38a4c48227e22910f60a423ba10ba7689de7236a
tranminhdang <minhdangtrancntt@gmail.com>
2021-03-27 10:39:55 +0700

    finsh-project

---
+++ src/test/java/com/minhdang/springsecurity/SpringBootChatBotMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhdang.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChatBotMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
